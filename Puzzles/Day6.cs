dfde1a8ba5ef7ca0148d90f398ada80262c89f7a
Jacek Stec (JTK) <JTK@kmd.dk>
2019-12-06 14:40:46 +0100

    added day6

---
+++ Puzzles/Day6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles
+{
+    public class Day6
+    {
+        public async Task Run()
+        {
+
+        }
+    }
+}
